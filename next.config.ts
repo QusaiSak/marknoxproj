46b71abc61376f16dc14398b39286ef522fa5dc0
QusaiSak <144017972+QusaiSak@users.noreply.github.com>
2024-11-18 08:30:02 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Ensures a static export
+  images: {
+    unoptimized: true, // Disables image optimization for static export
+  },
 };
 
 export default nextConfig;
